9a14459d5060c2c939d42d3db725bb5f974038ad
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-07-07 01:10:14 +0530

    fixed the bug in get-all-emp api

--- src/main/java/org/emp/service/impl/EmployeeServiceImpl.java
+++ src/main/java/org/emp/service/impl/EmployeeServiceImpl.java
@@ -6,7 +6,9 @@ import org.emp.dto.Employee;
 import org.emp.entity.EmployeeEntity;
 import org.emp.repository.EmployeeRepository;
 import org.emp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,27 +18,30 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     final EmployeeRepository repository; //constructor injection
 
-    List<Employee> employeeList = new ArrayList();
+    @Autowired
+    ObjectMapper mapper;
 
     @Override
     public Employee addEmployee(Employee employee) {
         EmployeeEntity entity = new ObjectMapper().convertValue(employee, EmployeeEntity.class);
         repository.save(entity);
-        return new ObjectMapper().convertValue(entity,Employee.class);
+        return new ObjectMapper().convertValue(entity, Employee.class);
     }
 
     @Override
     public List<Employee> getAllEmployees() {
-        List<EmployeeEntity> employeeEntities = repository.findAll();
-        employeeEntities.forEach(employeeEntity -> {
-            employeeList.add(new ObjectMapper().convertValue(employeeEntity,Employee.class));
+        Iterable<EmployeeEntity> allEmployees = repository.findAll();
+        List<Employee> employeeList = new ArrayList<>();
+        allEmployees.forEach(employeeEntity -> {
+            Employee employee = mapper.convertValue(employeeEntity, Employee.class);
+            employeeList.add(employee);
         });
         return employeeList;
     }
 
     @Override
     public boolean deleteEmployeeById(Long id) {
-        if (repository.existsById(id)){
+        if (repository.existsById(id)) {
             repository.deleteById(id);
             return true;
         }
@@ -45,18 +50,18 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee updateEmployee(Employee employee) {
-        if (repository.findById(employee.getId()).isPresent()){
+        if (repository.findById(employee.getId()).isPresent()) {
             EmployeeEntity entity = new ObjectMapper().convertValue(employee, EmployeeEntity.class);
             repository.save(entity);
-            return new ObjectMapper().convertValue(entity,Employee.class);
+            return new ObjectMapper().convertValue(entity, Employee.class);
         }
         return null;
     }
 
     @Override
     public Employee findEmployeeById(Long id) {
-        if (repository.findById(id).isPresent()){
-            return new ObjectMapper().convertValue(repository.findById(id),Employee.class);
+        if (repository.findById(id).isPresent()) {
+            return new ObjectMapper().convertValue(repository.findById(id), Employee.class);
         }
         return null;
     }
